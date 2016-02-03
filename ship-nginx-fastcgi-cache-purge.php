0b9c1185a6020ac178c89248852eea21911b96b5
Richard Sweeney <theorobman@gmail.com>
2016-02-03 14:55:35 +0100

    Better plugin headers, add t10ns

--- ship-nginx-fastcgi-cache-purge.php
+++ ship-nginx-fastcgi-cache-purge.php
@@ -1,12 +1,11 @@
 <?php
 /**
  * Plugin Name: Ship NGINX FastCGI cache purge
- * Version: 0.1-alpha
- * Description: PLUGIN DESCRIPTION HERE
- * Author: YOUR NAME HERE
- * Author URI: YOUR SITE HERE
- * Plugin URI: PLUGIN SITE HERE
- * Text Domain: ship-nginx-fastcgi-cache-purge
+ * Version: 0.1
+ * Description: A plugin to flush the NGINX FastCGI cache
+ * Author: The Shipyard Crew
+ * Author URI: https://theshipyard.se/
+ * Text Domain: sihp_nfcp
  * Domain Path: /languages
  * @package Ship NGINX FastCGI cache purge
  */
